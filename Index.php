26af1b250b43343e410176bcce337a97a22100a8
Tristan Lilly <112178449+TristanLilly@users.noreply.github.com>
2022-10-19 17:11:46 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    
+Today, every student has a computer small enough to fit into their <?php echo $_POST["Noun"]; ?><br>
+They can solve any math problem by simply pushing the computer's little <?php echo $_POST["pluralNoun"]; ?><br>
+Computers can add, multiply, divide, and <?php echo $_POST["Verb1"]; ?><br>
+They can also <?php echo $_POST["Verb2"]; ?> better than a human. <br>
+That is why they <?php echo $_POST["Adjective"]; ?> computers.<br>
+
+</body>
+</html>
